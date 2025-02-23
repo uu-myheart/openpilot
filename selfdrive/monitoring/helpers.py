16aa0b32c7afb58e319a798b1448f30b1ea00f11
GaranPoe <baigali@100tal.com>
2025-02-23 20:32:10 +0800

    Fix previous commit

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -365,6 +365,7 @@ class DriverMonitoring:
       #self.terminal_time += 1
       #if awareness_prev > 0.:
       #  self.terminal_alert_cnt += 1
+      alert = None
     elif self.awareness <= self.threshold_prompt:
       # prompt orange alert
       alert = EventName.promptDriverDistracted if self.active_monitoring_mode else EventName.promptDriverUnresponsive
