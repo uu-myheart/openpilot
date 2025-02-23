58a0af12ddeeda173df690f877adfb6cd49bd4f7
GaranPoe <baigali@100tal.com>
2025-02-23 11:08:30 +0530

    Do not disengage

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -361,10 +361,10 @@ class DriverMonitoring:
     alert = None
     if self.awareness <= 0.:
       # terminal red alert: disengagement required
-      alert = EventName.driverDistracted if self.active_monitoring_mode else EventName.driverUnresponsive
-      self.terminal_time += 1
-      if awareness_prev > 0.:
-        self.terminal_alert_cnt += 1
+      #alert = EventName.driverDistracted if self.active_monitoring_mode else EventName.driverUnresponsive
+      #self.terminal_time += 1
+      #if awareness_prev > 0.:
+      #  self.terminal_alert_cnt += 1
     elif self.awareness <= self.threshold_prompt:
       # prompt orange alert
       alert = EventName.promptDriverDistracted if self.active_monitoring_mode else EventName.promptDriverUnresponsive
